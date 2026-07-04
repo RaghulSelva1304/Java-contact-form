47296326399c091a2a8b337e7c562a6425b435f4
RaghulSelva1304 <rghlselva@gmail.com>
2026-07-04 22:40:50 +0530

    Completed Contact Form with Spring Boot and MySQL

--- Demo/src/main/java/Newuser/Demo/ContactController.java
+++ Demo/src/main/java/Newuser/Demo/ContactController.java
@@ -1,24 +1,36 @@
 package Newuser.Demo;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ContactController {
+
+    @Autowired
+    private ContactRepository contactRepository;
 
     @PostMapping("/contact")
     public String submitForm(
             @RequestParam String name,
             @RequestParam String email,
-            @RequestParam String message) {
+            @RequestParam String message
+    ) {
 
-        System.out.println("========== FORM DATA ==========");
-        System.out.println("Name: " + name);
-        System.out.println("Email: " + email);
-        System.out.println("Message: " + message);
-        System.out.println("===============================");
+        Contact contact = new Contact();
+
+        contact.setName(name);
+        contact.setEmail(email);
+        contact.setMessage(message);
+
+        contactRepository.save(contact);
 
         return "Form Submitted Successfully!";
+    }
+
+    @GetMapping("/contacts")
+    public List<Contact> getAllContacts() {
+        return contactRepository.findAll();
     }
 }
---
+++ Demo/src/main/java/Newuser/Demo/ContactRepository.java
@@ -0,0 +1,7 @@
+package Newuser.Demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+}
